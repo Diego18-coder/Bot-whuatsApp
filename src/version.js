55c649beac507fb25e2c7f3672f006764eb3ba67
Diego18-coder <85782888+Diego18-coder@users.noreply.github.com>
2021-08-05 02:22:40 -0400

    Add files via upload

---
+++ src/version.js
@@ -0,0 +1,25 @@
+const version = (prefix, pushname) => {
+    return `
+*Diego Mod*
+
+*Actualizado:* 25 de julio de 2021
+*Versión actual:* 3.2
+*Ofrecida por:* Diego Mod
+
+
+*INFORME*
+
+Si no funciona el comando *play o *play2 checa el blog del bot y mira la version que poses 
+Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
+Cambiare la apikey pasando un mes o si veo muchos atercados o robos
+Si vez a alguien que robo mi bot comunicate conmigo con el comando *creador
+
+
+
+
+Diego Mod
+`
+
+}
+
+exports.version = version
